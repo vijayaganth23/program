aaef05181ebd682a88701fc844a73d1cff3575d4
Vijayaganth R <vijayaganth.r@gmail.com>
2017-06-20 17:02:51 +0530

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int num;
+ clrscr()
+ printf("\nEnter the number:");
+ scanf("%d",&num)
+ if(n>0)
+  printf("\n The given number is positive");
+ elseif(n<0)
+  printf("\n The given number is negative");
+   else
+     printf("\n The given number is Zero");
+  }
