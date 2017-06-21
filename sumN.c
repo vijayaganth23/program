60a1d911c26ba0ba1fb00069f7304295aecc9729
Vijayaganth R <vijayaganth.r@gmail.com>
2017-06-21 13:17:26 +0530

    Create sumN.c

---
+++ sumN.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int n,i,sum=0,num;
+ printf("\nEnter the N value:");
+ for(i=1;i<=n;i++)
+ {
+  scanf("%d",&num);
+  sum=sum+num;
+  }
+  printf("sum=%d",sum);
+  return 0;
+  }
