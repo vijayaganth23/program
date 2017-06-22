52bdbcf5daa393f57b6eda05509d16b14db5c5f0
Vijayaganth R <vijayaganth.r@gmail.com>
2017-06-22 12:22:18 +0530

    Create palin.c

---
+++ palin.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+ int num,rem,n;
+ int sum=0;
+ printf("\nEnter the number:");
+ scanf("%d",&num);
+ n=num;
+ while(n>0)
+ {
+  rem=num%10;
+  sum=sum*10+rem;
+  num=num/10;
+  }
+  if(n==sum)
+    printf("\n Given number is palindrome");
+  else
+   printf("\n Given number is not palindrome");
+  return 0;
+  }
+  
+   
