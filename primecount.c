bcfc385189fa7494dd5b2d906ad898349e41027e
Vijayaganth R <vijayaganth.r@gmail.com>
2017-06-23 13:22:59 +0530

    Update primecount.c

--- primecount.c
+++ primecount.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
 int main()
 {
- int num,i,flag;
+ int num,i,j,flag,count=0;
  scanf("%d",&num);
- for(i=3;i<=n;i++)
+ for(i=3;i<=num;i=i+2)
  {
   flag=0;  
-  for(j=3;j<=num/2;j+=2)
+  for(j=2;j<i;j++)
   {
    if(i%j==0)
      flag=1;
    }
-   if(flag==1)
+   if(flag==0)
+   {
     printf("%d",i);
+    count++;
   }
+ }
+ printf("%d",count+2);
   return 0;
   }
     
