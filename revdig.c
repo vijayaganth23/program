6dd574fc5f1afe0767a160f37905967f13562af6
Vijayaganth R <vijayaganth.r@gmail.com>
2017-06-23 12:37:02 +0530

    Create revdig.c

---
+++ revdig.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int num,revno,rem;
+ scanf("%d",&num);
+ revno=0;
+ while(num>=0)
+ {
+  rem=num%10;
+  revno=revno*10+rem;
+  num=num/10;
+ }
+ printf("%d",revno);
+ return 0;
+ }
+  
