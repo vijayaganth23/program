d91d926bca3242c29b4a447a2272b33be654d84c
Vijayaganth R <vijayaganth.r@gmail.com>
2017-07-15 09:23:54 +0530

    Create leaveornot.cpp

---
+++ leaveornot.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ string str;
+ cin>>str;
+ if(str=="sunday")
+  cout<<"Holiday"<<endl;
+ else
+  cout<<"Working day"<<endl;
+ return 0;
+ }
